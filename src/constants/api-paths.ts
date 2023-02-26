44514148512ed564dc8f03547d6613ab81280838
Dmytro Romanenko <dmytro.romanenko@wickes.co.uk>
2023-02-26 23:55:30 +0200

    changed api urls, integrated with BE

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
-	product: 'https://.execute-api.eu-west-3.amazonaws.com/dev',
-	order: 'https://.execute-api.eu-west-3.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-3.amazonaws.com/dev',
-	bff: 'https://.execute-api.eu-west-3.amazonaws.com/dev',
-	cart: 'https://.execute-api.eu-west-3.amazonaws.com/dev',
+	product: 'https://u7ctc7g8o0.execute-api.eu-west-3.amazonaws.com/dev',
+	order: 'https://u7ctc7g8o0.execute-api.eu-west-3.amazonaws.com/dev',
+	import: 'https://u7ctc7g8o0.execute-api.eu-west-3.amazonaws.com/dev',
+	bff: 'https://u7ctc7g8o0.execute-api.eu-west-3.amazonaws.com/dev',
+	cart: 'https://u7ctc7g8o0.execute-api.eu-west-3.amazonaws.com/dev',
 };
