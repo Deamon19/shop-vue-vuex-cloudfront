4602369380906c0aef12e7fd410e794617aea5b0
Dmytro Romanenko <dmytro.romanenko@wickes.co.uk>
2023-04-02 18:14:07 +0300

    task-7

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://ddg2066n9l.execute-api.eu-west-3.amazonaws.com/dev',
 	order: 'https://ddg2066n9l.execute-api.eu-west-3.amazonaws.com/dev',
-	import: 'https://ddg2066n9l.execute-api.eu-west-3.amazonaws.com/dev',
+	import: 'https://b8j5mivqkd.execute-api.eu-west-3.amazonaws.com/dev',
 	bff: 'https://ddg2066n9l.execute-api.eu-west-3.amazonaws.com/dev',
 	cart: 'https://ddg2066n9l.execute-api.eu-west-3.amazonaws.com/dev',
 };
